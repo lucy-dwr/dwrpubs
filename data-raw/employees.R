4df4e8de3ff8b316e6048673fe75ffc8872b3146
Lucy Andrews <lucy.andrews@water.ca.gov>
2025-10-31 14:56:12 -0700

    add column for employee nicknames/alternate names

--- data-raw/employees.R
+++ data-raw/employees.R
@@ -9,13 +9,20 @@
 
 # Columns in this file:
 # - employee_name: full name of the employee
+# - employee_alternate_name: alternate name/spelling for the employee
 # - division_adjusted: division within DWR where the employee worked in
 #     the given year
 # - year: year of employment record
 # - division_old: old division name for divisions whose identity has changed
 
-employees <- readr::read_csv(here::here("data-raw", "dwr_employee_division_year.csv")) |>
-  dplyr::mutate(employee_name = stringr::str_to_title(employee_name)) |>
+employees <- readr::read_csv(
+  here::here("data-raw", "dwr_employee_division_year.csv"),
+  col_types = "cccc"
+) |>
+  dplyr::mutate(
+    employee_name = stringr::str_to_title(employee_name),
+    employee_alternate_name = stringr::str_to_title(employee_alternate_name)
+  ) |>
   unique()
 
 usethis::use_data(employees, overwrite = TRUE)
